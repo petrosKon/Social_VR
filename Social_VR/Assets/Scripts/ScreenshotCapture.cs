12989f217b79da9aa4a8caa7c541a75138436273
Kontrazis Petros <55915800+petrosKon@users.noreply.github.com>
2021-05-16 23:04:05 +0200

    Screenshot capture

---
+++ Social_VR/Assets/Scripts/ScreenshotCapture.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotCapture : MonoBehaviour
+{
+    public GameObject screenshotQuad;
+    public RenderTexture cameraRenderTexture;
+    private Camera thisCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCamera = GetComponent<Camera>();
+        cameraRenderTexture = thisCamera.targetTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            StartCoroutine(TakeSnapshot());
+        }
+    }
+
+    public IEnumerator TakeSnapshot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        thisCamera.Render();
+
+        Texture2D image = new Texture2D(cameraRenderTexture.width, cameraRenderTexture.height, TextureFormat.RGB24, true);
+        image.ReadPixels(new Rect(0, 0, cameraRenderTexture.width, cameraRenderTexture.height), 0, 0, false);
+        image.Apply();
+
+
+        screenshotQuad.GetComponent<Renderer>().material.mainTexture = image;
+    }
+}
--- Social_VR/Assets/Scripts/WebCamDisplay.cs
+++ Social_VR/Assets/Scripts/WebCamDisplay.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WebCamDisplay : MonoBehaviour
+public class WebCamDisplay : MonoBehaviour, IWebCamDisplay
 {
     // Start is called before the first frame update
     void Start()
+    {
+        AttachWebcamTexture();
+    }
+
+    public void AttachWebcamTexture()
     {
         WebCamTexture webCamTexture = new WebCamTexture();
         this.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
