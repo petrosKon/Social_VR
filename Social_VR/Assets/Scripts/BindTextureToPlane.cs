46be4bc81c83b6be4927d301d1661b7115430b63
Rached Waly <rachedwaly1998@gmail.com>
2021-05-17 12:13:36 +0200

    screenShot is repaired

---
+++ Social_VR/Assets/Scripts/BindTextureToPlane.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindTextureToPlane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Renderer myplane_renderer;
+    private void Awake()
+    {
+        myplane_renderer = gameObject.GetComponent<Renderer>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Texture my_img = (Texture)Resources.Load("zall");
+        myplane_renderer.material.mainTexture = my_img;
+
+    }
+}
---
+++ Social_VR/Assets/Scripts/Handler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handler : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            ScreenShot.TakeScreenshot_static(500, 500);
+        }
+    }
+}
